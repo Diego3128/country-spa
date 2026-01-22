42ab674290070458c37324208981404cb4b0ebb0
Diego Perez <145170018+Diego3128@users.noreply.github.com>
2026-01-22 14:38:58 -0500

    feat: trigger search as the input value changes   - use fromEvent to control the input search

--- src/app/country/components/country-search/country-search.ts
+++ src/app/country/components/country-search/country-search.ts
@@ -1,11 +1,12 @@
-import { Component, input, output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, input, OnDestroy, output, viewChild } from '@angular/core';
+import { debounceTime, fromEvent, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-country-search',
   imports: [],
   templateUrl: './country-search.html',
 })
-export class CountrySearch {
+export class CountrySearch implements AfterViewInit, OnDestroy {
   newSearch = output<string>(); //event emitter
 
   placeholder = input<string>('Search');
@@ -14,4 +15,32 @@ export class CountrySearch {
     if (!value) return;
     this.newSearch.emit(value);
   };
+
+  textInputRef = viewChild<ElementRef<HTMLInputElement>>("txtSearch");
+
+  subscription?: Subscription;
+
+
+  ngAfterViewInit(): void {
+    // clean prev
+    if (this.subscription && !this.subscription.closed) this.subscription.unsubscribe();
+    //create observable
+    const textSource = fromEvent<InputEvent>(this.textInputRef()?.nativeElement!, 'input');
+    // create observer and get subscription
+    this.subscription = textSource.pipe(
+      debounceTime(1000),
+      map((inputEvent) => {
+        const target = inputEvent.target as HTMLInputElement | null;
+        return target?.value.toLowerCase() ?? '';
+      })
+    ).subscribe({
+      next: (value) => {
+        console.log({ value });
+        this.onSearch(value);
+      }
+    })
+  }
+  ngOnDestroy(): void {
+    if (this.subscription && !this.subscription.closed) this.subscription.unsubscribe();
+  }
 }
